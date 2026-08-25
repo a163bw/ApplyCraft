57189d75736aa8c4b4fde9caa8a767fcc4d7079c
a163 <ge78yez@tum.de>
2026-08-25 22:14:33 +0200

    clear

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-"""ApplyCraft deterministic CV generator package."""
-
-from .generator import generate_cv_latex, load_json, validate_and_resolve_inputs
-
-__all__ = ["generate_cv_latex", "load_json", "validate_and_resolve_inputs"]
